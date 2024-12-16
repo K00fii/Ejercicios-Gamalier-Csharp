6b0ff55e9a267bb5c1d0ac50eb803a29d8822a6d
K00fii <junioracevedovaldez@gmail.com>
2024-12-16 01:31:23 -0400

    Ejercicios con Bucles

---
+++ Bucles/Contraseña/Contraseña/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Contraseña correcta definida
+        string contraseñaCorrecta = "12345";
+        string contraseñaIngresada;
+        int intentosRestantes = 3;
+
+        // Ciclo para permitir hasta 3 intentos
+        while (intentosRestantes > 0)
+        {
+            Console.WriteLine("Ingresa su contraseña:");
+            contraseñaIngresada = Console.ReadLine();
+
+            // Verificar si la contraseña es correcta
+            if (contraseñaIngresada == contraseñaCorrecta)
+            {   
+                Console.WriteLine("Bienvenido otra vez");
+                break; // Salir del ciclo si la contraseña es correcta
+            }
+            else
+            {
+                intentosRestantes--; // Reducir el número de intentos restantes
+                if (intentosRestantes > 0)
+                {
+                    Console.WriteLine($"Contraseña incorrecta. {intentosRestantes} intentos restantes.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Fuera kbron!!");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Bucles/NumerosEnteros/NumerosEnteros/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+
+        int cantidadNumero, numero;
+        int positivos = 0, negativos = 0, pares = 0, inpares = 0;
+
+        Console.WriteLine("Introduzca la cantidad");
+        cantidadNumero = Convert.ToInt32(Console.ReadLine());
+
+
+
+        for (int i = 1; i <= cantidadNumero; i++)
+        {
+
+            Console.WriteLine("Introzca un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El conteo se detuvo");
+                break;
+
+            }
+
+            if (numero > 0)
+            {
+
+                positivos++;
+
+            }
+
+            else if (numero < 0)
+
+            { 
+
+            negativos++;
+
+            }
+
+
+
+            if (numero % 2 == 0)
+            {
+
+                pares++;
+
+            }
+
+            else
+            {
+                inpares++;
+            }
+        
+        
+        
+        
+        
+        }
+
+
+
+        Console.WriteLine($"Cantidad de numeros positivos {positivos}");
+        Console.WriteLine($"Cantidad de numeros negativos {negativos}");
+        Console.WriteLine($"Cantidad de numeros pares {pares}");
+        Console.WriteLine($"Cantidad de numeros inpares {inpares}");
+
+    }
+
+
+
+}
